8a4784518c5cc3054b1f5a4333ddc3d82914d6d1
Bryce Mecum <petridish@gmail.com>
2018-07-16 23:05:33 -0800

    Try making the copied _book output folder not a subfolder

--- _build_and_merge.R
+++ _build_and_merge.R
@@ -21,7 +21,7 @@ for (tag in tags) {
                           clean_envir = FALSE)
     
     message("Copying _book folder from ", getwd(), " to ", file.path(top, "public", "materials", material, tag))
-    file.copy("_book", file.path(top, "public", "materials", material, tag), recursive = TRUE)
+    file.copy("_book", file.path(top, "public", "materials", paste0(material, "-", tag)), recursive = TRUE)
 
     message("Materials folder contains:", dir(file.path(top, "public", "materials")))
     unlink("_book", recursive = TRUE)
